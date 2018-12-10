a34dd1e934abc80a6abbdf779dc36e001a8ab805
Munna Circuit <munna@iMac-9.local>
2018-12-10 16:32:23 -0500

    initial commit

---
+++ src/main/java/auto/rem/config/ConfigApplication.java
@@ -0,0 +1,26 @@
+package auto.rem.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class ConfigApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigApplication.class, args);
+	}
+
+	@GetMapping("/user")
+	public String getUser() {
+		return "not a config server user";
+	}
+
+	@GetMapping("/password")
+	public String getPassword() {
+		return "not a config server password";
+	}
+}
---
+++ src/test/java/auto/rem/config/ConfigApplicationTests.java
@@ -0,0 +1,16 @@
+package auto.rem.config;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConfigApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
